15037032c213a317002f4e15296005a8b6380797
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-07-24 20:50:01 +0300

    map equiped

--- src/pages/indexCard/subcomponents/UnitComposition.jsx
+++ src/pages/indexCard/subcomponents/UnitComposition.jsx
@@ -5,24 +5,38 @@ const UnitComposition = () => {
   const composition = useSelector(
     (state) => state.app.indexCard.unit_composition
   );
+  const equiped = useSelector((state) => state.app.indexCard.equiped);
   return (
     <span>
-      {console.log(composition)}
       <div className="indexCard__unit-composition">unit composition</div>
       {composition.length > 1 ? (
         <div>
           <ul className="indexCard__unit-composition-list">
-            <li className="font-negrita font-rules">1 Intercessor Sergeant</li>
-            <li className="font-negrita font-rules">4-9 Intercessors</li>
+            {/* <li className="font-negrita font-rules">1 Intercessor Sergeant</li>
+            <li className="font-negrita font-rules">4-9 Intercessors</li> */}
+            {composition &&
+              composition.map((c, i) => (
+                <li className="font-negrita font-rules">{c}</li>
+              ))}
           </ul>
           <p className="font-rules">
-            <span className="font-negrita">Every model is equipped with:</span>
-            bolt pistol; bolt rifle; close combat weapon.
+            {composition.length > 1 && (
+              <span className="font-negrita">
+                Every model is equipped with:
+              </span>
+            )}
+            {equiped.map((e, i) => (
+              <span className="font-rules">
+                {" "}
+                {e}
+                {i !== equiped.length - 1 ? ", " : "."}
+              </span>
+            ))}
           </p>
           <div className="indexCard__divisor"></div>
         </div>
       ) : (
-        <div>mama guevo!!!</div>
+        <div>{composition}</div>
       )}
     </span>
     // {composition.lenght > 1 &&   }
