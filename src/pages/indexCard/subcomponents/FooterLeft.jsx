e62bbcf0c42506a1bf59016727553ee74e178b8f
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-07-25 01:02:17 +0300

    keywords

--- src/pages/indexCard/subcomponents/FooterLeft.jsx
+++ src/pages/indexCard/subcomponents/FooterLeft.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const FooterLeft = () => {
+  const keywords = useSelector((state) => state.app.indexCard.keywords);
   return (
     <div className="indexCard__footer-left">
       <p>
-        <span className="font-slim-tall">keywords:</span> Infantry, Character,
-        Epic Hero, Imperium, Terminator, Arjac Rockfist
+        <span className="font-slim-tall">keywords: </span>
+        {keywords.map((k, i) => (
+          <span key={i}>
+            {k}
+            {i !== keywords.length - 1 ? ", " : "."}
+          </span>
+        ))}
       </p>
     </div>
   );
--- src/pages/indexCard/subcomponents/FooterRight.jsx
+++ src/pages/indexCard/subcomponents/FooterRight.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const FooterRight = () => {
+  const keywords = useSelector((state) => state.app.indexCard.faction_keywords);
   return (
     <div className="indexCard__footer-right font-negrita font-rules">
       <p>
-        <span className="font-slim-tall">faction keywords:</span> Adeptus
-        Astartes, Space Wolves
+        <span className="font-slim-tall">faction keywords: </span>
+        {keywords.map((k, i) => (
+          <span key={i}>
+            {k}
+            {i !== keywords.length - 1 ? ", " : "."}
+          </span>
+        ))}
       </p>
     </div>
   );
