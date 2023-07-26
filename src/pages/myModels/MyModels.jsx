9de318db578968ceec21cb9ca1dff02407581849
Victor-python <64462520+Victor-python@users.noreply.github.com>
2023-07-26 23:54:30 +0300

    ideas of search and army counter

--- src/pages/myModels/MyModels.jsx
+++ src/pages/myModels/MyModels.jsx
@@ -8,9 +8,24 @@ const MyModels = () => {
   const dispatch = useDispatch();
   return (
     <div className="my-models">
+      <div>
+        <input type="text" placeholder="Search" />{" "}
+        <label htmlFor="">Order by: </label>
+        <select>
+          <option value="pts">name</option>
+          <option value="pts">cheapest</option>
+          <option value="pts">expensier</option>
+          <option value="pts">movement</option>
+        </select>
+      </div>
+
       {testData &&
         testData.map((data, i) => (
-          <div className="my-models__card" key={i}>
+          <div
+            className="my-models__card"
+            key={i}
+            onClick={() => dispatch(addToArmy(data))}
+          >
             <p className="my-models__card-text">{data.point_cost[0].cost}pts</p>
             <p className="my-models__card-text">
               {data.point_cost[0].amount > 1
@@ -20,7 +35,7 @@ const MyModels = () => {
             </p>
 
             <ModelsImages imgName={data.image} type={"listedModel"} />
-            <button onClick={() => dispatch(addToArmy(data))}>add</button>
+            <div>0/3</div>
           </div>
         ))}
     </div>
