5f2414f4c82d20023c6d7af58b30e8c863268a79
Victor <victorgrinan@gmail.com>
2024-11-06 21:13:42 +0200

    loop the func

--- data_xml/xml_to_json.py
+++ data_xml/xml_to_json.py
@@ -9,7 +9,7 @@ def convert_xml_to_json(input_file, output_filename):
 
     dict_data = xmltodict.parse(xml_data) 
     json_data = json.dumps(dict_data, indent=4)
-    json_data = json_data.replace('@', '').replace('#', '')
+    json_data = json_data.replace('@', '').replace('#', '').replace('\u00e2\u20ac\u02dc', "'").replace('\u00e2\u20ac\u2122', "'").replace('\u00c2\u00a0', " ")
 
     with open(output_filename, 'w') as js_file:
         js_file.write(f"const data = {json_data};")
@@ -18,11 +18,18 @@ def convert_xml_to_json(input_file, output_filename):
     print(f"Deleted original XML file: {input_file}")
 
 if __name__ == "__main__":
-    filename = 'Imperium - Space Wolves'
-    # input_file = r'C:\Users\victo\Desktop\Imperium - Space Wolves.cat'
-    input_file = r'C:\Users\victo\Desktop\Warhammer 40,000.cat'
-    'Imperium - Space Wolves.cat'
-    print(input_file)
-  
-    output_filename = './src/data/Warhammer 4k.js'
-    convert_xml_to_json(input_file, output_filename)
+    files = [
+        'Imperium - Space Wolves.cat',
+        'Imperium - Space Marines.cat',
+        'Imperium - Dark Angels.cat',
+        'Imperium - Astra Militarum.cat',
+        'Imperium - Ultramarines.cat',
+        'Tyranids.cat',
+        'Warhammer 40,000.gst'
+
+    ]
+    for file in files:
+        filename = '\\' + file
+        input_file = r'C:\Users\victo\Desktop' + filename
+        output_filename = './src/data/'+ file + '.js'
+        convert_xml_to_json(input_file, output_filename)
