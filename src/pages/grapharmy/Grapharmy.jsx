11352385e2277999ea56719db253ccf8e96a23af
Victor <victorgrinan@gmail.com>
2024-11-13 08:30:39 +0200

    Grapharmy

--- src/pages/grapharmy/Grapharmy.jsx
+++ src/pages/grapharmy/Grapharmy.jsx
@@ -1,17 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import UnitCard from "../../components/unitCard/UnitCard";
 import { useDispatch, useSelector } from "react-redux";
 import { setSelectedUnit } from "../../app/appSlice";
+import { useNavigate } from "react-router-dom";
 
 const Grapharmy = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const army = useSelector((state) => state.app.army);
-  const [data, setData] = useState(false);
 
   const selectModel = (modelsProps) => {
     dispatch(setSelectedUnit(modelsProps))
   };
 
+  useEffect(() => {
+    if(army.armyList.length === 0){
+      navigate('/mymodels')
+    }
+  }, [army.armyList]);
+  
   return (
     <div className="grapharmy">
 
