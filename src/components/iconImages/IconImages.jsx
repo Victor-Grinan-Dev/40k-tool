87147e4560d9a54eb78555472f9d7106dae1b778
Victor <victorgrinan@gmail.com>
2024-11-15 14:21:25 +0200

    create IconImages

--- src/components/iconImages/IconImages.jsx
+++ src/components/iconImages/IconImages.jsx
@@ -6,7 +6,7 @@ import battleline from '../../assets/unittypes/battleline-modified.png';
 import fast from '../../assets/unittypes/fast-removebg-preview.png';
 import heavy from '../../assets/unittypes/heavy-removebg-preview.png';
 
-const IconImages = () => {
+const IconImages = ({imgName, type}) => {
     const images = {
         elite:elite,
         character:character,
@@ -15,12 +15,12 @@ const IconImages = () => {
         heavy:heavy,
     }
 
-    // const imagesTypes = {
-    //     // listedModel: "my-models__card-image",
-    //     // unit: "unit-card__image",
-    //     // selected: "modelCard__model-image",
-    //     // index: "indexCard__model-image",
-    //   };
+    const imagesTypes = {
+        // listedModel: "my-models__card-image",
+        // unit: "unit-card__image",
+        // selected: "modelCard__model-image",
+        // index: "indexCard__model-image",
+      };
 
   return (
     <img src={images[imgName]} alt={imgName} className={imagesTypes[type] || null} />
